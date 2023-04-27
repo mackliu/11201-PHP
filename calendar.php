df406d49837f3bb6832e52cc83d1825b63530da6
mackliu <macklun@gmail.com>
2023-04-27 15:24:13 +0800

    簡化月曆需要的變數,並修改前後月的使用方式

--- calendar.php
+++ calendar.php
@@ -1,15 +1,13 @@
 <link rel="stylesheet" href="style.css">
 
-
 <h2>月曆</h2>
 <?php
-$month=(isset($_GET['month']))?$_GET['month']:date("n");    //取得當前的月份
-$today=strtotime("now");    //取得當前的時間秒數
-$days=date("t",$today);     //取得當前月份的總天數
-$firstDate=date("Y-n-1",$today);    //取得當前月份第一天
-$finalDate=date("Y-n-t",$today);    //取得當前月份最後一天
-$firstDateWeek=date("w",strtotime($firstDate)); //取得當前月份第一天的星期
-$finalDateWeek=date("w",strtotime($finalDate)); //取得當前月份最後一天的星期
+$month=$_GET['month']??date("n");    //取得當前的月份
+$firstDateTime=strtotime(date("Y-".$month."-1"));    //取得當前月份第一天
+$days=date("t",$firstDateTime);     //取得當前月份的總天數
+$finalDateTime=strtotime(date("Y-n-t",$firstDateTime));    //取得當前月份最後一天
+$firstDateWeek=date("w",$firstDateTime); //取得當前月份第一天的星期
+$finalDateWeek=date("w",$finalDateTime); //取得當前月份最後一天的星期
 $weeks=ceil(($days+$firstDateWeek)/7);  //計算當前月份的天數會佔幾周
 $firstWeekSpace=$firstDateWeek-1;       //計算當前月份第一周的空白日(或前一個月份佔幾天)
 $days=[];
@@ -27,14 +25,17 @@ for($i=0;$i<$weeks;$i++){
     }
 }
 
+$prevMonth=$month-1;
+$nextMonth=$month+1;
+
 ?>
 
 <div>
-<a href="prev.php"><?=$month-1;?>月</a>
+<a href="calendar.php?month=<?=$prevMonth;?>"><?=$prevMonth;?>月</a>
 
 <span><?=$month;?>月</span>
 
-<a href="next.php?month=<?=$month+1;?>"><?=$month+1;?>月</a>
+<a href="calendar.php?month=<?=$nextMonth;?>"><?=$nextMonth;?>月</a>
 </div>
 <div class='calendar'>
 <div>日</div>
